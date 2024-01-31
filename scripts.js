b3e560bd240b4f9b4724bd20484c945d96fa2afd
Victor Terra <victorbterra@gmail.com>
2024-01-31 13:29:45 -0300

    :tada: initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Inicio da funcao do botao toggle//
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // Buscando imagem na tag //
+  const img = document.querySelector("#profile img")
+  // Caso a condicao no html for light //
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Victor Terra em um fundo branco")
+    // Caso a condicao nao for light//
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("alt", "Foto de Victor Terra de óculos e camisa azul")
+  }
+}
